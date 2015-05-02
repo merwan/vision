4f029b1be9965187fbf0a65015cf3efd81fdf40d
Merouane Atig <merouane.atig@gmail.com>
2015-05-02 14:50:36 +0200

    Add middleware for 404

--- test/heartbeat.js
+++ test/heartbeat.js
@@ -10,4 +10,13 @@ describe('vision heartbeat api', function() {
       .expect(200, done);
     });
   });
+
+  describe('when requesting resource /missing', function() {
+    it('should respond with 404', function(done) {
+      request(app)
+      .get('/missing')
+      .expect('Content-Type', /json/)
+      .expect(404, done);
+    });
+  });
 });
